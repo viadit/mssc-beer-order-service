c9cb6b1cb08d486c51e512a0fbde30941630cf36
Vishal <vishal.aditya6@gmail.com>
2021-11-22 19:44:45 +0530

    Setting port to 8081. closes #1

--- src/main/java/guru/springframework/msscbeerorderservice/web/mappers/BeerOrderMapper.java
+++ src/main/java/guru/springframework/msscbeerorderservice/web/mappers/BeerOrderMapper.java
@@ -3,12 +3,14 @@ package guru.springframework.msscbeerorderservice.web.mappers;
 import guru.springframework.msscbeerorderservice.domain.BeerOrder;
 import guru.springframework.msscbeerorderservice.web.model.BeerOrderDto;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import java.util.Date;
 
 @Mapper(uses = {DateMapper.class, BeerOrderLineMapper.class})
 public interface BeerOrderMapper {
 
+    @Mapping(target = "customerId", source = "customer.id")
     BeerOrderDto beerOrderToDto(BeerOrder beerOrder);
 
     BeerOrder dtoToBeerOrder(BeerOrderDto dto);
